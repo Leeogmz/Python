13041fe0d3aa3d2ef1ad65a369fdd9909e7b04d6
Leonardo <leonarddo.gomees@gmail.com>
2023-05-15 19:17:18 -0300

    Python

---
+++ Python para Data Science Primeiros Passos/variaveis.py
@@ -0,0 +1,22 @@
+quantidade_seguranca = 5
+salario_seguranca = 3000
+
+quantidade_docente = 16 
+salario_docente = 6000
+
+quantidade_diretoria = 1
+salario_diretoria = 12500
+
+
+total_empregados = quantidade_diretoria + quantidade_docente + quantidade_seguranca
+print(total_empregados)
+
+total_salario = quantidade_seguranca * salario_seguranca + quantidade_docente * salario_docente + quantidade_diretoria * salario_diretoria
+
+diferenca_salario  = salario_diretoria - salario_seguranca
+print(diferenca_salario)
+
+media = total_salario / total_empregados
+
+
+print(media)
