bcee46de0db347eedc568d9ca5aad46ea351cbf5
Leonardo <leonarddo.gomees@gmail.com>
2023-05-16 17:43:52 -0300

    Realizando input e conversão de dados

--- Python para Data Science Primeiros Passos/variaveis.py
+++ Python para Data Science Primeiros Passos/variaveis.py
@@ -7,20 +7,14 @@ salario_docente = 6000
 quantidade_diretoria = 1
 salario_diretoria = 12500
 
-empresa = 'Alura'
-empresa2 = 'Alura'
-
 professora = ' Geovana Alessandra dias Sanyos '
-print(professora)
 
 
 total_empregados = quantidade_diretoria + quantidade_docente + quantidade_seguranca
-print(total_empregados)
 
 total_salario = quantidade_seguranca * salario_seguranca + quantidade_docente * salario_docente + quantidade_diretoria * salario_diretoria
 
 diferenca_salario  = salario_diretoria - salario_seguranca
-print(diferenca_salario)
 
 media = total_salario / total_empregados
 
@@ -30,4 +24,11 @@ print(media)
 professora = professora.strip().replace('y', 't').upper()
 
 print(professora)
+
+nome = input('Escreva seu nome: ')
+
+ano_entrada = int(input('Escreva o ano de ingresso do(a) estudante: '))
+
+print(f'Nome do aluno: {nome} - Ano de entrada: {ano_entrada}')
+Le
 
