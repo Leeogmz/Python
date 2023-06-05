d0a72cac0335b5c1f058352762abefffbf7e4637
Leonardo <leonarddo.gomees@gmail.com>
2023-06-04 22:27:57 -0300

    Exercitando

--- Exercicios.py
+++ Exercicios.py
@@ -29,3 +29,14 @@ for i in range(len(lista3)):
     lista_de_tuplas2.append((i, lista3[i]))
 print(lista_de_tuplas2) 
 
+
+#4) Crie uma lista usando o list comprehension que armazena somente o valor numérico de cada tupla caso o primeiro elemento seja 'Apartamento', a partir da seguinte lista de tuplas:
+
+aluguel = [('Apartamento', 1700), ('Apartamento', 1400), ('Casa', 2150), ('Apartamento', 1900), ('Casa', 1100)]
+
+lista4 = [tupla[1] for tupla in aluguel if tupla[0]== 'Apartamento']
+print(lista4)
+
+
+
+
