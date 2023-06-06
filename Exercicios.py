de394ed1bdd2ec0240221c44bb855e509d95f853
Leonardo <leonarddo.gomees@gmail.com>
2023-06-06 18:04:06 -0300

    Praticando

--- Exercicios.py
+++ Exercicios.py
@@ -51,3 +51,50 @@ vendas = [('2023', 4093), ('2021', 4320), ('2021', 5959), ('2022', 8883), ('2023
 
 filtro = [tupla[1] for tupla in vendas if tupla[0] == '2022' and tupla[1] > 6000]
 print(filtro)
+
+
+""" 7) Uma clínica analisa dados de pacientes e armazena o valor numérico da glicose em um banco de dados e gostaria de rotular os dados da seguinte maneira:
+Glicose igual ou inferior a 70: 'Hipoglicemia'
+Glicose entre 70 a 99: 'Normal'
+Glicose entre 100 e 125: 'Alterada'
+Glicose superior a 125: 'Diabetes'
+A clínica disponibilizou parte dos valores e sua tarefa é criar uma lista de tuplas usando list comprehension contendo o rótulo e o valor da glicemia em cada tupla."""
+
+dados2 = [129, 82, 60, 97, 101, 65, 62, 167, 87, 53, 58, 92, 66, 120, 109, 62, 86, 96, 103, 88, 155, 52, 89, 73] 
+
+rotulos = [('Hipoglicemia', glicemia) if glicemia <= 70 else
+           ('Normal', glicemia) if 70 < glicemia <= 99 else
+           ('Alterada', glicemia) if 100 <= glicemia <= 125 else
+           ('Diabetes', glicemia) for glicemia in dados2]
+
+
+print(rotulos)
+
+""" 8) Um e-commerce possui as informações de id de venda, quantidade vendida e preço do produto divididos nas listas a baixo 
+ O e-commerce precisa estruturar esses dados em uma tabela contendo o valor total da venda, 
+ que é obtida multiplicando a quantidade pelo preço unitário. Além disso, a tabela precisa conter um cabeçalho indicando as colunas: 'id', 'quantidade', 'preco' e 'total'.
+
+Crie uma lista de tuplas em que cada tupla tenha um id, quantidade, preço e valor total, na qual a primeira tupla é o cabeçalho da tabela. """
+
+id = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+quantidade = [15, 12, 1, 15, 2, 11, 2, 12, 2, 4]
+preco = [93.0, 102.0, 18.0, 41.0, 122.0, 14.0, 71.0, 48.0, 14.0, 144.0]
+
+tabela = [('id', 'quantidade', 'preco', 'total')]
+tabela += [(id[i], quantidade[i], preco[i], quantidade[i]*preco[i]) for i in range(len(id))]
+
+print(tabela)
+
+
+""" 9) Uma empresa possui filiais espalhadas nos Estados da região Sudeste do Brasil. Em uma das tabelas de cadastro das filiais há uma coluna contendo a informação de qual é o Estado a que pertence
+
+A empresa sempre está abrindo novas filiais, de modo que a tabela está constantemente recebendo novos registros e o gestor gostaria de possuir a informação atualizada da quantidade de filiais em cada Estado.
+
+A partir da coluna com a informação dos Estados, crie um dicionário usando dict comprehension com a chave sendo o nome de um Estado e o valor sendo a contagem de vezes em que o Estado aparece na lista. """
+
+estados = ['SP', 'ES', 'MG', 'MG', 'SP', 'MG', 'ES', 'ES', 'ES', 'SP', 'SP', 'MG', 'ES', 'SP', 'RJ', 'MG', 'RJ', 'SP', 'MG', 'SP', 'ES', 'SP', 'MG']
+
+
+ocorrencias_estados = {estado: estados.count(estado) for estado in set(estados)}
+
+print(ocorrencias_estados)
