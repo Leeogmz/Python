988a02b7de1446672845a15f707a9bceb00adcec
Leonardo <leonarddo.gomees@gmail.com>
2023-06-16 19:23:49 -0300

    Verificar as dimensões de um array; Realizar a transposição de um array.

--- Numpy/apples_ts.py
+++ Numpy/apples_ts.py
@@ -1,4 +1,14 @@
 import numpy as np
 
+caminho='C:\\Users\\leona\OneDrive\\Documentos\\Estudos\\Python\\Numpy\\numpy-dados\\apples_ts.csv'
+dado = np.loadtxt(caminho, delimiter=',', usecols=np.arange(1,88, 1))
+#print(dado)
 
-print(np.loadtxt('apples_ts.csv', delimiter=',', usecols=np.arange(1,88, 1)))
+print(dado.ndim) #para saber as dimensões da tabela 2d
+print(dado.size) #Verifica a quantidade de elemtos do array
+print(dado.shape) #Verifica o numero de elementos em cada dimensão 
+
+
+dado_transposto =  dado.T #para transpor os elemntos
+
+print(dado_transposto)
--- Numpy/numpy-dados/apples_ts.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-caminho='C:\\Users\\leona\OneDrive\\Documentos\\Estudos\\Python\\Numpy\\numpy-dados\\apples_ts.csv'
-
-print(np.loadtxt(caminho, delimiter=',', usecols=np.arange(1,88, 1)))
