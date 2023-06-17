cd2ec397da91d538c90b8f1e086561059e45a78e
Leonardo <leonarddo.gomees@gmail.com>
2023-06-17 19:20:54 -0300

    Visualizando graficos com matplotlib

--- Numpy/apples_ts.py
+++ Numpy/apples_ts.py
@@ -1,14 +1,29 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 caminho='C:\\Users\\leona\OneDrive\\Documentos\\Estudos\\Python\\Numpy\\numpy-dados\\apples_ts.csv'
 dado = np.loadtxt(caminho, delimiter=',', usecols=np.arange(1,88, 1))
 #print(dado)
 
-print(dado.ndim) #para saber as dimensões da tabela 2d
-print(dado.size) #Verifica a quantidade de elemtos do array
-print(dado.shape) #Verifica o numero de elementos em cada dimensão 
+#print(dado.ndim) #para saber as dimensões da tabela 2d
+#print(dado.size) #Verifica a quantidade de elemtos do array
+#print(dado.shape) #Verifica o numero de elementos em cada dimensão 
 
 
 dado_transposto =  dado.T #para transpor os elemntos
 
-print(dado_transposto)
+datas = dado_transposto[:,0] #para separar a primeira coluna
+datas = np.arange(1,88, 1)
+precos = dado_transposto [:,1:6]
+
+
+Moscow = precos[:,0]
+Kaliningrad = precos[:,1]
+Petersburg = precos[:,2]
+Krasnodar = precos[:,3]
+Ekaterinburg = precos[:,4]
+
+
+grafico = plt.plot(datas, Kaliningrad)
+plt.show()
+
