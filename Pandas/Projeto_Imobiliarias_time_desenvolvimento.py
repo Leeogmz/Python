b3a6c4c995c1aeabe2c0840bd2b6acf92ee072e0
Leonardo <leonarddo.gomees@gmail.com>
2023-07-08 20:20:56 -0300

    salvando os dados

--- Pandas/Projeto_Imobiliarias_time_desenvolvimento.py
+++ Pandas/Projeto_Imobiliarias_time_desenvolvimento.py
@@ -11,8 +11,9 @@ dados['Valor_por_ano'] = dados['Valor_por_mes'] * 12 + dados['IPTU']
 
 dados['Descricao'] = dados['Tipo'] + ' em ' +  dados['Bairro'] + ' com ' + dados['Quartos'].astype(str) + ' quarto(s) ' + ' e ' + dados ['Vagas'].astype(str) + ' vaga(s) de garagem.'
 
+dados['Possui_suite'] = dados['Suites'].apply(lambda x: 'Sim' if x > 0 else 'Não')
+
 dados.to_csv('dados_desenvolvimento.csv', index=False, sep=';')
 
-dados['Possui_suite'] = dados['Suites'].apply(lambda x: 'Sim' if x > 0 else 'Não')
 
 print(dados.head())
