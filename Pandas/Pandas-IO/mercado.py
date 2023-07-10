95c39b6f43c9da914027b470ef356770271cd902
Leonardo <leonarddo.gomees@gmail.com>
2023-07-10 14:50:32 -0300

    Salvando CSV com pandas

--- Pandas/Pandas-IO/mercado.py
+++ Pandas/Pandas-IO/mercado.py
@@ -6,5 +6,12 @@ dados_mercado = pd.read_csv(url, sep=',')
 
 dados_selecao = pd.read_csv(url, usecols=['Id', 'Year_Birth', 'Income'])
 
+dados_selecao.to_csv('dados_mercado.csv', sep=';', index=False)
+
+
+clientes_mercado = pd.read_csv('dados_mercado.csv', sep=';')
+
 print(dados_mercado.head())
 print(dados_selecao.head())
+print(clientes_mercado.head())
+
