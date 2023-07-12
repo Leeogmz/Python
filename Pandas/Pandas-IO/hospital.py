bca14c20afc567814ac551f3b89ab91073ac6c7e
Leonardo <leonarddo.gomees@gmail.com>
2023-07-12 20:32:01 -0300

    normalizando arquivos json

--- Pandas/Pandas-IO/hospital.py
+++ Pandas/Pandas-IO/hospital.py
@@ -2,5 +2,14 @@ import pandas as pd
 
 dados_pacientes = pd.read_json('Pandas\Pandas-IO\data\pacientes.json')
 
+dados_pacientes_2 = pd.read_json('Pandas\Pandas-IO\data\pacientes_2.json')
+
+df_normalizado = pd.json_normalize(dados_pacientes_2['Pacientes'])
+
+
+
 print(dados_pacientes.head())
+print(dados_pacientes_2.head())
+print(df_normalizado.head())
+
 
