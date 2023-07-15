e18e5823b89361fc9d6b8bd05fee185f714f8df7
Leonardo <leonarddo.gomees@gmail.com>
2023-07-15 20:23:13 -0300

    Obtendo o data frame com os top filmes

--- Pandas/Pandas-IO/filmes_sugestoes.py
+++ Pandas/Pandas-IO/filmes_sugestoes.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-
 dados_html = pd.read_html('C:\\Users\\leona\OneDrive\\Documentos\\Estudos\\Python\\Pandas\\Pandas-IO\\data\\filmes_wikipedia.html')
 
-print(dados_html)
+top_filmes = dados_html[1]
+
+print(top_filmes)
