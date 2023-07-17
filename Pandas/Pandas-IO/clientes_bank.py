48d98e2dce8181f657308bff363b423d05347484
Leonardo <leonarddo.gomees@gmail.com>
2023-07-17 18:55:01 -0300

    fazendo consultas SQL ao banco

--- Pandas/Pandas-IO/clientes_bank.py
+++ Pandas/Pandas-IO/clientes_bank.py
@@ -1,15 +1,27 @@
+
+import sqlite3
 import pandas as pd
 import sqlalchemy 
 from sqlalchemy import create_engine, MetaData, Table, inspect
 
-engine = create_engine('sqlite:///:memory:')
+
+engine = create_engine('sqlite:///data_base.db')
 
 url = 'https://raw.githubusercontent.com/alura-cursos/Pandas/main/clientes_banco.csv'
 
 dados = pd.read_csv(url)
 
-dados.to_sql('Clientes', index=False)
+#dados.to_sql('Clientes',con=engine, index=False)
 
 inspector = inspect(engine)
 
-print(inspector.get_table_names())
+
+query = 'select * from Clientes where Categoria_de_renda = "Empregado"'
+
+empregados = pd.read_sql(query, engine)
+
+#empregados.to_sql('Empregados',con=engine,index=False)
+
+leitura = pd.read_sql_table('Empregados', engine)
+
+print(leitura)
