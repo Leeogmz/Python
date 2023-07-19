68d6fdf66e88569201ab02d5660a0230de822ca4
Leonardo <leonarddo.gomees@gmail.com>
2023-07-19 20:14:08 -0300

    Deletando registros do banco de dados

--- Pandas/Pandas-IO/clientes_bank.py
+++ Pandas/Pandas-IO/clientes_bank.py
@@ -11,6 +11,7 @@ url = 'https://raw.githubusercontent.com/alura-cursos/Pandas/main/clientes_banco
 
 dados = pd.read_csv(url)
 
+#Para Criar a tabela no banco de dados
 #dados.to_sql('Clientes',con=engine, index=False)
 
 inspector = inspect(engine)
@@ -20,8 +21,14 @@ query = 'select * from Clientes where Categoria_de_renda = "Empregado"'
 
 empregados = pd.read_sql(query, engine)
 
+#Para Criar a tabela no banco de dados
 #empregados.to_sql('Empregados',con=engine,index=False)
 
 leitura = pd.read_sql_table('Empregados', engine)
 
-print(leitura)
+deletando_usuario = sqlalchemy.text('DELETE FROM Clientes WHERE ID_Cliente = 5008804')
+with engine.connect() as conn:
+    result = conn.execute(deletando_usuario)
+    conn.commit()    
+    print("Registros excluídos:", result.rowcount)
+
