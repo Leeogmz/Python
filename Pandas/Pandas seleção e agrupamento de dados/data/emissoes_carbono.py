c47bfc828b83ea84d963c7ad98069710f3313deb
Leonardo <leonarddo.gomees@gmail.com>
2023-07-21 21:19:07 -0300

    filtrando apenas as emissoes de gases co2

--- Pandas/Pandas seleção e agrupamento de dados/data/emissoes_carbono.py
+++ Pandas/Pandas seleção e agrupamento de dados/data/emissoes_carbono.py
@@ -4,6 +4,18 @@ caminho = 'C:\\Users\\leona\\OneDrive\\Documentos\\Estudos\\Python\\Pandas\\Pand
 
 emissoes_gases = pd.read_excel(caminho, sheet_name='GEE Estados')
 
-#print(emissoes_gases.head())
+#print(emissoes_gases['Emissão / Remoção / Bunker'].unique())
 
-print(emissoes_gases.info())
+#print((emissoes_gases['Emissão / Remoção / Bunker'] == 'Remoção NCI') | (emissoes_gases['Emissão / Remoção / Bunker'] == 'Remoção')) 
+
+#emissoes_gases_remocao = emissoes_gases.loc[emissoes_gases['Emissão / Remoção / Bunker'].isin(['Remoção NCI','Remoção']), 1970:2021].max()
+
+emissoes_gases_bunker = emissoes_gases.loc[emissoes_gases['Emissão / Remoção / Bunker'] == 'Bunker', 'Estado'].unique()
+
+emissoes_gases_final = emissoes_gases[emissoes_gases['Emissão / Remoção / Bunker'] == 'Emissão']
+
+emissoes_gases_final = emissoes_gases.drop(columns= 'Emissão / Remoção / Bunker')
+
+
+
+print(emissoes_gases_final)
