4f689a5283730d796a327e21beeb28221f21a87a
Leonardo <leonarddo.gomees@gmail.com>
2023-08-06 16:44:22 -0300

    retirando as listas

--- Pandas/Pandas transformação e manipulação de dados/precificacao_imoveis.py
+++ Pandas/Pandas transformação e manipulação de dados/precificacao_imoveis.py
@@ -2,4 +2,12 @@ import pandas as pd
 
 dados_hospedagem = pd.read_json('Pandas\\Pandas transformação e manipulação de dados\\data\\dados_hospedagem.json')
 
+dados_hospedagem = pd.json_normalize(dados_hospedagem['info_moveis'])
+
+colunas = list(dados_hospedagem.columns)
+
+dados_hospedagem = dados_hospedagem.explode(colunas[3:])
+
+dados_hospedagem.reset_index(inplace=True, drop=True)
+
 print(dados_hospedagem)
