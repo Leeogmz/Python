0393136cdec86ea113287ac63368c9e0235b6549
Leonardo <leonarddo.gomees@gmail.com>
2023-08-07 18:43:09 -0300

    Convertendo dados numéricos

--- Pandas/Pandas transformação e manipulação de dados/precificacao_imoveis.py
+++ Pandas/Pandas transformação e manipulação de dados/precificacao_imoveis.py
@@ -1,4 +1,5 @@
 import pandas as pd 
+import numpy as np
 
 dados_hospedagem = pd.read_json('Pandas\\Pandas transformação e manipulação de dados\\data\\dados_hospedagem.json')
 
@@ -9,5 +10,13 @@ colunas = list(dados_hospedagem.columns)
 dados_hospedagem = dados_hospedagem.explode(colunas[3:])
 
 dados_hospedagem.reset_index(inplace=True, drop=True)
+
+col_numericas = ['quantidade_banheiros', 'quantidade_quartos', 'quantidade_camas', 'max_hospedes']
+
+dados_hospedagem[col_numericas] = dados_hospedagem[col_numericas].astype(np.int64)
+
+dados_hospedagem['avaliacao_geral'] = dados_hospedagem['avaliacao_geral'].astype(np.float64)
+
+print(dados_hospedagem.info())
 
 print(dados_hospedagem)
