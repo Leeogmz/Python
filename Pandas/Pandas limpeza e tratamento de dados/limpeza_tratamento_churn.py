eb567182c1f1c0457d05fdda10dac0adad4d663d
Leonardo <leonarddo.gomees@gmail.com>
2023-08-13 16:24:51 -0300

    Modificando o tipo da coluna

--- Pandas/Pandas limpeza e tratamento de dados/limpeza_tratamento_churn.py
+++ Pandas/Pandas limpeza e tratamento de dados/limpeza_tratamento_churn.py
@@ -8,6 +8,20 @@ with open('Pandas\\Pandas limpeza e tratamento de dados\\data\\dataset-telecon.j
 
 dados_normalizados = pd.json_normalize(json_bruto)
 
+dados_normalizados['conta.contrato'][2075] = 'dois anos'
+
+idx = dados_normalizados[dados_normalizados['conta.cobranca.Total']==' '].index
+
+dados_normalizados.loc[idx, 'conta.cobranca.Total'] = dados_normalizados.loc[idx, 'conta.cobranca.mensal'] * 24
+
+dados_normalizados.loc[idx, 'cliente.tempo_servico'] = 24
+
+print(dados_normalizados.loc[idx][
+    ['cliente.tempo_servico','conta.contrato' ,'conta.cobranca.mensal','conta.cobranca.Total' ]
+    ])
+
+dados_normalizados['conta.cobranca.Total'] = dados_normalizados['conta.cobranca.Total'].astype('float64') 
+
 dados_normalizados.info()
 
 print(dados_normalizados.head())
