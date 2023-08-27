2fa77fd518e8c7c9d03b949595949bd945765fc4
Leonardo <leonarddo.gomees@gmail.com>
2023-08-27 10:48:23 -0300

    Distribuição de frequências qualitativas

--- Estatística com Python/Frequências e medidas/estatistica.py
+++ Estatística com Python/Frequências e medidas/estatistica.py
@@ -10,3 +10,29 @@ dados = pd.read_csv('C:\\Users\\leona\\OneDrive\\Documentos\\Estudos\\Python\\Es
 
 print(dados.head())
 dados.info()
+
+print(sorted(dados['Anos de Estudo'].unique()))
+
+print(sorted(dados['UF'].unique()))
+
+print(sorted(dados['Sexo'].unique()))
+
+print(sorted(dados['Cor'].unique()))
+
+print('A idade varia de %s até %s anos' % (dados['Idade'].min(),dados['Idade'].max()))
+
+#Distribuição de frequências para variáveis qualitativas
+
+frequencia = dados['Sexo'].value_counts()
+
+percentual = dados['Sexo'].value_counts(normalize=True) * 100
+
+dist_freq_qualitativas = pd.DataFrame({'Frequencia': frequencia, 'Porcentagem (%)':percentual})
+
+dist_freq_qualitativas.rename(index={0: 'Masculino' , 1: 'Feminino'}, inplace=True)
+
+dist_freq_qualitativas.rename_axis('Sexo', axis='columns',inplace=True)
+
+print(frequencia)
+print(percentual)
+print(dist_freq_qualitativas)
