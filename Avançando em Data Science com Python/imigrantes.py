62e398259ee20f9b0b78c079cdea2a653f18d024
Leonardo Gomes <leonarddo.gomees@gmail.com>
2024-04-23 09:33:29 -0300

    Organizando o repositorio

--- Avançando em Data Science com Python/imigrantes.py
+++ Avançando em Data Science com Python/imigrantes.py
@@ -22,4 +22,26 @@ plt.xticks(['1980','1985', '1990','1995','2000','2005','2010'])
 plt.title ('Imigração do Brasil para o Canadá')
 plt.xlabel('Ano')
 plt.ylabel ('Número de imigrantes')
+
+df_comparacao = df.loc[['Brasil', 'Argentina'], anos]
+
+df_comparacao = df_comparacao.T
+
+plt.plot(df_comparacao['Brasil'],label = 'Brasil')
+plt.plot(df_comparacao['Argentina'],label ='Argentina')
+plt.title('Imigração do Brasil e Argentina para o Canadá')
+plt.xlabel('Ano')
+plt.ylabel('Número de imigrantes')
+plt.xticks(['1980', '1985', '1990', '1995', '2000', '2005', '2010'])
+plt.legend()
+
+#Criando figura
+
+fig, ax = plt.subplots(figsize=(8,4))
+ax.plot(dados_brasil['ano'], dados_brasil['imigrantes'])
+ax.set_title('Imigração do Brasil para o Canadá\n1980 a 2013')
+ax.set_xlabel('Ano')
+ax.set_ylabel('Número de imigrantes')
+ax.xaxis.set_major_locator(plt.MultipleLocator(5))
+
 plt.show()
