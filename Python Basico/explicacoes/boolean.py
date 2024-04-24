5d510e92e2dd1d63ca7ca965e36df278f9bdab23
Leonardo Gomes <leonarddo.gomees@gmail.com>
2024-04-24 19:26:53 -0300

    boolean com python

---
+++ Python Basico/explicacoes/boolean.py
@@ -0,0 +1,27 @@
+2 > 3 #Mentira, False
+3 > 2 #Verdade, True
+
+True
+False
+type(True)
+
+nome_1 = 'Fausto'
+idade_1 = 3
+nome_2 = 'MAria'
+idade_2 = 23
+nome_3 = 'Fausto'
+idade_3 = 45
+
+nome_1 == nome_2 #False
+
+nome_1 == nome_3 #True
+
+not nome_1 == nome_3 #inverte
+
+nome_1 != nome_2 #True
+
+nome_1 == nome_3 and idade_1 == idade_3 #False
+
+nome_1 == nome_3 and idade_1 == idade_1 #True
+
+nome_1 == nome_3 or idade_1 == idade_3 #True
