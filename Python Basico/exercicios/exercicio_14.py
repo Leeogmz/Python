4a99a1faf33ee8fe680f7895ef8bb7912bb07997
Leonardo Gomes <leonarddo.gomees@gmail.com>
2024-04-30 17:16:42 -0300

    exercicio sobre listas

---
+++ Python Basico/exercicios/exercicio_14.py
@@ -0,0 +1,16 @@
+"""Exercício #14
+Faça uma programa que dada a entrada de uma lista ele faça o cálculo
+acumulativo da mesma:
+Exemplo de entrada: [1, 2, 3, 4]
+Exemplo de saída: [1, 3, 6, 10]"""
+
+entrada = [1,2,3,4]
+saida = []
+soma = 0
+
+for elemento in entrada:
+    soma += elemento
+    saida.append(soma)
+    print(saida)
+     
+ 
