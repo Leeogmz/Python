14bc47a1da64b406cd8ebee0670cc9807558b173
Leonardo Gomes <leonarddo.gomees@gmail.com>
2024-05-04 21:42:59 -0300

    Criando subplots em duas direções

--- Avançando em Data Science com Python/imigrantes.py
+++ Avançando em Data Science com Python/imigrantes.py
@@ -44,7 +44,7 @@ ax.set_xlabel('Ano')
 ax.set_ylabel('Número de imigrantes')
 ax.xaxis.set_major_locator(plt.MultipleLocator(5))
 
-
+# criando subplots em uma direção
 fig, axs = plt.subplots(1,2, figsize=(15,5))
 axs[0].plot(dados_brasil['ano'], dados_brasil['imigrantes'])
 axs[0].set_title('Imigração do Brasil para o Canadá\n1980 a 2013')
@@ -58,6 +58,38 @@ axs[1].set_title('Boxplot da imigração do Brasil para o Canadá\n1980 a 2013')
 axs[1].set_xlabel('Brasil')
 axs[1].set_ylabel('Número de imigrantes')
 axs[1].grid()
-plt.show()
 
 print(dados_brasil.describe())
+
+# criando subplots em duas direção
+
+fig, axs = plt.subplots(2,2, figsize=(10,6))
+fig.subplots_adjust(hspace=0.5, wspace=0.3)
+fig.suptitle('Imigração dos quatro maiores países da América do Sul para o Canadá de 1980 a 2013')
+
+axs[0,0].plot(df.loc['Brasil', anos])
+axs[0,0].set_title('Brasil')
+
+axs[0,1].plot(df.loc['Colômbia', anos])
+axs[0,1].set_title('Colômbia')
+
+axs[1,0].plot(df.loc['Argentina', anos])
+axs[1,0].set_title('Argentina')
+
+axs[1,1].plot(df.loc['Peru', anos])
+axs[1,1].set_title('Peru')
+
+for ax in axs.flat:
+    ax.xaxis.set_major_locator(plt.MultipleLocator(5))
+
+for ax in axs.flat:
+    ax.set_xlabel('Ano')
+    ax.set_ylabel('Número de imigrantes')    
+
+ymin = 0
+xmax = 7000
+
+for ax in axs.ravel():
+    ax.set_ylim(ymin,xmax)  
+    
+plt.show()
