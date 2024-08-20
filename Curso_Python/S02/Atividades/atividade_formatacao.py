ef339ae80f34e786b1a20a02d9221e31b6cd4e2d
leeogmz <leonarddo.gomees@gmail.com>
2024-08-20 08:25:53 -0300

    Operadores aritmeticos

---
+++ Curso_Python/S02/Atividades/atividade_formatacao.py
@@ -0,0 +1,20 @@
+#1° 
+
+dia = 15
+mes = 12
+ano = 1997
+
+print(dia, mes, ano, sep='/')
+
+#2° 
+
+hora = 7
+minuto = 58
+
+print(f'Agora são: {hora} horas e {minuto} minutos')
+
+#3° 
+
+pi = 3.14159265359
+
+print('O PI é %.5f' % (pi))
