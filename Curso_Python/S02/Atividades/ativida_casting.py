2a08d90a1f1f31574e24ce0298e64cf18a03dc85
leeogmz <leonarddo.gomees@gmail.com>
2024-08-25 08:14:32 -0300

    Atividade casting

---
+++ Curso_Python/S02/Atividades/ativida_casting.py
@@ -0,0 +1,11 @@
+#1º
+saldo = "30000"
+saldo = (float(saldo) - 1000)
+print("Meu saldo bancario é de: ", saldo )
+
+#2º 
+print('Há saldo na minha conta? ', bool(saldo))
+
+#3º
+altura = 1.70
+print('A parte inteira da minha altura é: ', int(altura))
