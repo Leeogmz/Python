f60141752b3f28f1a760728dba768a7682648adf
leeogmz <leonarddo.gomees@gmail.com>
2024-09-03 22:12:20 -0300

    Operadores sobre String

--- Curso_Python/S02/fundamentos.py
+++ Curso_Python/S02/fundamentos.py
@@ -339,3 +339,30 @@ var = "Exemplo"
 print(var[1:3])
 print(var[3:])
 print(var[:5])
+
+#Operadores sobre String
+
+texto1 = "Olá"
+texto2 = ", "
+texto3 = "tudo bem? "
+texto_completo = texto1 + texto2 + texto3
+print(texto_completo)
+
+texto = "Python é bem produtivo, "
+texto_repetido = texto * 3
+print(texto_repetido)
+
+texto = "Exemplo"
+print(texto[0])
+print(texto[1])
+
+var2 = "carro"
+print(var2[-4])
+print(var2[-4:])
+print(var2[:-1])
+print(var2[-5:-2])
+
+var3 = "metro"
+print(var3[::-1])
+print(var3[3::-1])
+print(var3[3:1:-1])
