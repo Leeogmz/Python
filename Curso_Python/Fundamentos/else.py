113dc25d0745b88a020373a2dadb6731fae2d458
leeogmz <leonarddo.gomees@gmail.com>
2024-09-07 16:31:28 -0300

    else

---
+++ Curso_Python/Fundamentos/else.py
@@ -0,0 +1,30 @@
+#Else
+
+numero = 9
+if numero >= 10:
+    print('É maior ou igual a 10!')
+    print('___________')
+else: 
+    print('É menor que 10!')
+    print('___________')
+print('Fim')
+
+
+numero = 10
+if numero % 2 == 0:
+    print('É par!')
+else: 
+    print('É impar!')
+
+valor = 5
+if valor >=0 and valor < 10: 
+    print(valor)
+else:
+    print('Valor não aceito')
+
+
+texto = 'ab'
+if len (texto) == 1:
+    print('Tem 1 caractere')
+else: 
+    print('Não tem 1 caractere')
