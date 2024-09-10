c439ea6d96c07ae6792615b4c4a96befa95b10b3
leeogmz <leonarddo.gomees@gmail.com>
2024-09-09 21:55:10 -0300

    aninhamento de ifs

--- Curso_Python/Fundamentos/aninhamento_ifs.py
+++ Curso_Python/Fundamentos/aninhamento_ifs.py
@@ -22,3 +22,25 @@ elif numero <= 1000:
     print('menor ou igual a 1000')
 elif numero < 10:
     print('Nenhuma anterior')
+
+texto = 'b'
+if texto == 'a':
+    print('É vogal a')
+elif texto == 'e':
+    print('É vogal e')
+elif texto == 'i':
+    print('É vogal i')
+elif texto == 'o':
+    print('É vogal o')
+elif texto == 'u':
+    print('É vogal u')
+else: 
+    print('É consoante')
+
+numero = 15
+resultado = 'PAR' if numero % 2 == 0 else 'IMPAR'
+print(resultado)
+
+numero = 3
+resultado = 'Um' if numero == 1 else 'Dois' if numero == 2 else 'Três'
+print(resultado)
