a84b52d7e703a781a153ce611d2a8a571f711e61
leeogmz <leonarddo.gomees@gmail.com>
2024-09-12 20:33:23 -0300

    complemento atividade IF

--- Curso_Python/Atividades/atividade_if.py
+++ Curso_Python/Atividades/atividade_if.py
@@ -19,3 +19,40 @@ if senha_input == senha:
     print(cpf)
 else: 
     print('Senha incorreta! ')
+
+#3° 
+
+idade = int(input('Digite sua idade: '))
+ 
+if idade <= 3:
+   print('Você ainda é um bebe')
+elif idade <= 13:
+    print('Você é uma criança')
+elif idade <= 18:
+    print('Você é um adolescente')
+elif idade <= 65:
+    print('Você é um adulto')
+else: 
+    print('Você é um idoso')  
+    
+
+#4°
+
+nume1 = float(input('Digite o primeiro número: '))
+nume2 = float(input('Digite o segundo número: '))
+operacao = input('Digite a operação: ')    
+
+if (operacao == "+"):
+    soma = nume1 + nume2
+    print("A soma é: ", soma) 
+elif (operacao == "-"):
+    subtracao = nume1 - nume2
+    print("A subtração é ", subtracao) 
+elif (operacao == "*") :
+    multiplicacao = nume1 * nume2
+    print("A multiplição é ", multiplicacao) 
+elif (operacao == "/"): 
+    divisao = nume1 / nume2
+    print("A divisão é ", divisao)
+else:
+    print("Operação Inválida!")
