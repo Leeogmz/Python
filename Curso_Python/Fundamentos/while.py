55332495bf4eac50f7e8f5d8adb5b01572a3d494
leeogmz <leonarddo.gomees@gmail.com>
2024-09-14 21:06:08 -0300

    while parte 2

--- Curso_Python/Fundamentos/while.py
+++ Curso_Python/Fundamentos/while.py
@@ -16,5 +16,20 @@ while (indice >= 2):
     indice -= 1
 print('Programa encerrou!')
 
+soma = 0
+numeros_lidos = 0
 
-    
+while (numeros_lidos < 5):
+    num = float(input('Digite um valor: '))
+    soma += num
+    numeros_lidos += 1
+
+print('Soma é: %.2f' % soma)
+
+
+texto = 'Olá 123 _'
+indice = 0
+
+while (indice < len(texto)):
+    print(texto[indice])
+    indice += 1
