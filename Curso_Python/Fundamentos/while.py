97e6fcf7a8b751abbc69dfc4acb11cd6efeb5a66
leeogmz <leonarddo.gomees@gmail.com>
2024-09-17 18:41:52 -0300

    break e continue

--- Curso_Python/Fundamentos/while.py
+++ Curso_Python/Fundamentos/while.py
@@ -33,3 +33,21 @@ indice = 0
 while (indice < len(texto)):
     print(texto[indice])
     indice += 1
+
+num_atual = 0
+
+while (True):
+    if num_atual == 5:
+        break
+    print(num_atual)
+    num_atual +=1
+print('Encerrou')        
+
+num_atual = 0
+
+while num_atual < 10:
+    num_atual += 1
+    if num_atual == 7:
+        continue
+    print(num_atual)
+print('Encerrou')    
