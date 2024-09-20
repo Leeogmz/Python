d4b449d15fbeb1c8d386496386d043d3ac33c02e
leeogmz <leonarddo.gomees@gmail.com>
2024-09-20 20:15:24 -0300

    for

---
+++ Curso_Python/Fundamentos/for.py
@@ -0,0 +1,33 @@
+
+for x in range(10):
+    print(x)
+
+print('______________')
+for w in range(5,10):
+    print(w)      
+
+print('______________')
+for w in range(5,20,5):
+    print(w)    
+
+print('______________')
+for w in range(20,5,-5):
+    print(w)    
+
+print('______________')
+
+texto ='123456789'
+for x in range(0, len(texto)):
+    print(texto[x])
+
+print('______________')
+
+letra = input('Digite uma letra: ')
+
+if (len(letra) != 1):
+    print('Precisa ter 1 digito! ')
+else:
+    texto = input ('Digite um texto: ') 
+    for i in range(0,len(texto)):
+        if letra == texto[i]:
+            print('Encontei a letra " %s " na posição %d ' % (letra, i))
