a69a642bcfec5d4f2670c14209587ff1ce9da8db
leeogmz <leonarddo.gomees@gmail.com>
2024-09-27 22:56:05 -0300

    continuação listas

--- Curso_Python/Fundamentos/listas.py
+++ Curso_Python/Fundamentos/listas.py
@@ -26,3 +26,31 @@ print(array)
 array = [1, 'texto', 3]
 
 print(array[1:3])
+
+array = [10,2,3]
+print(array)
+array.append(2)
+print(array)
+array.insert(2, '4')
+print(array)
+
+array = [10,2,3,20,'3']
+array.remove(10)
+print(array)
+array.pop(2)
+print(array)
+print(len(array))
+
+array.clear()
+print(array)
+
+array = [1, 'teste', 1.3, True]
+
+print(1 in array)
+print(False not in array)
+
+lista = [5,6,7,2,3,4,7]
+teste = lista.count(7)
+print(teste)
+pos = lista.index(5)
+print(pos)
