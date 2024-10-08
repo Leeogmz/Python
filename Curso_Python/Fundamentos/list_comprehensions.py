92b121c9fed0d88d3630c415450504bc6fb0eea5
leeogmz <leonarddo.gomees@gmail.com>
2024-10-08 11:41:51 -0300

    list comprehension parte 2

--- Curso_Python/Fundamentos/list_comprehensions.py
+++ Curso_Python/Fundamentos/list_comprehensions.py
@@ -27,3 +27,16 @@ print(lista)
 
 lista = [ str(x) + ' par' if x % 2 == 0 else str(x) + ' impar' for x in range(2,11)]
 print(lista)
+
+lista = [1,2,3,4,5,6,7,8,9]
+lista = list(filter(lambda x: x % 2 == 0, lista))
+print(lista)
+
+lista = [ [x for x in range(1,5)] for y in range(1,5) ]
+print(lista)
+
+lista = [ [str(x) + str(y) for x in range(1,5)] for y in range(1,5) ]
+print(lista)
+
+lista = [[str(x) + '*' + str(y) + '=' + str(x*y) for x in range (1,5)] for y in range(1,11)]
+print(lista)
