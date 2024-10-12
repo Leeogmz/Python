7cea8a7dd9a453f4a08983a3b5d8d964a6a263ca
leeogmz <leonarddo.gomees@gmail.com>
2024-10-12 08:56:05 -0300

    Funções

---
+++ Curso_Python/Fundamentos/funcoes.py
@@ -0,0 +1,43 @@
+def func():
+    num = 10
+    print('Uma função %d' % (num))
+func()    
+
+def func():
+    pass
+func()
+
+def print_var (numero):
+    print(numero)
+
+print_var(2)    
+print('Maça')
+
+def print_soma(num1,num2):
+    print(num1+num2)
+
+print_soma(50,50)    
+print_soma(25.5,24.5)  
+print_soma('Olá ', 'mundo')
+
+def func(*args):
+    print(type(args))
+    print('Argumentos são: ', args)
+
+func(1,2,3)
+func()
+func('Olá', True, [1,2,3])
+
+def print_sub(num1,num2):
+    print(num1-num2)
+
+print_sub(10,2)  
+print_sub(num2=2,num1=10)      
+
+def func(*args, outro):
+    print('Argumentos são: ', args)
+    print(outro)
+
+func(1,2,3, outro=1)
+func(outro=2)        
+func('Olá', True, [1,2,3], outro=3)
