d4f55550f3787b73f4ef5f4920f3537b4e5a739d
leeogmz <leonarddo.gomees@gmail.com>
2024-10-23 11:54:06 -0300

    funções builtin

---
+++ Curso_Python/Fundamentos/funcoes_builtin.py
@@ -0,0 +1,31 @@
+num1 = -10
+num2 = 10
+print(abs(num1))
+print(abs(num2))
+print(abs(num1) + abs(num2))
+
+maior_valor = max(10,20,30)
+print(maior_valor)
+
+lista = [-20, 1,2.3,3]
+print(max(lista))
+print(min(lista))
+
+x = 2
+y = 3
+print(2**3)
+print(pow(x,y))
+
+import math
+
+print(math.sqrt(25))
+
+print(round(2.4566, 2))
+print(round(2.4566, 3))
+
+from math import floor, ceil
+
+print(floor(2.4566))
+print(ceil(2.4566))
+
+print(divmod(10,4))
